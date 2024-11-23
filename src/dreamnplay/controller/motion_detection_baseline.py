e87eae0d03bb710256c0a137f5d063cebca29bdd
Balthazar Neveu <balthazarneveu@gmail.com>
2024-11-23 06:03:49 +0100

    add real jump

--- src/dreamnplay/controller/motion_detection_baseline.py
+++ src/dreamnplay/controller/motion_detection_baseline.py
@@ -1,13 +1,18 @@
+from dreamnplay.training.model import MLPBaseline, WINDOW_SIZE
+from dreamnplay.training.dataset import CLASSES_NAMES
 import safetensors.torch
 import mediapipe as mp
 import time
 import torch
-import mediapipe as mp
+import numpy as np
 NOSE = mp.solutions.pose.PoseLandmark.NOSE.name
+TORSO_JOINTS = [
+    mp.solutions.pose.PoseLandmark.LEFT_SHOULDER,
+    mp.solutions.pose.PoseLandmark.RIGHT_SHOULDER,
+    mp.solutions.pose.PoseLandmark.LEFT_HIP,
+    mp.solutions.pose.PoseLandmark.RIGHT_HIP
+]
 
-
-from dreamnplay.training.dataset import CLASSES_NAMES
-from dreamnplay.training.model import MLPBaseline, WINDOW_SIZE
 
 keypoints_names = [e for e in mp.solutions.pose.PoseLandmark]
 labels2actions = {v: k for k, v in CLASSES_NAMES.items()}
@@ -43,7 +48,6 @@ class DetectorBaseline():
             self.current_action = None
             return
 
-
         # Convert to tensor
         positions = process_keypoints_list(self.all_kps).unsqueeze(0)
 
@@ -52,20 +56,26 @@ class DetectorBaseline():
             res = torch.softmax(res, dim=-1)
             index = res[0].argmax().item()
             action = labels2actions[index]
-        
+
         # window = 2
         prev_kp = self.all_kps[-3]
         prev_nose = prev_kp[NOSE]
         curr_nose = keypoint[NOSE]
-        
+        prev_torso = np.array(
+            [prev_kp[joint.name] for joint in TORSO_JOINTS]
+        ).mean(axis=0)
+        curr_torso = np.array(
+            [keypoint[joint.name] for joint in TORSO_JOINTS]
+        ).mean(axis=0)
+
         # if action == "JUMP":
-            # self.current_action = "JUMP"
+        # self.current_action = "JUMP"
         # print(curr_nose[1] - prev_nose[1])
         if action == "CROUCH":
             self.current_action = "CROUCH"
         elif action == "ACTIVATE":
             self.current_action = "ACTIVATE"
-        elif (curr_nose[1] - prev_nose[1]) < -0.005*3.:
+        elif (curr_torso[1] - prev_torso[1]) < -0.005*3.:
             self.current_action = "JUMP"
         else:
             # print("IDLE")
